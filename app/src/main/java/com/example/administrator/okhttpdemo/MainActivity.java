f72b94c8a6b708c51fb4984fb65071b21761c944
Administrator <123@123.com>
2018-02-24 18:36:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/okhttpdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.administrator.okhttpdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        Request.Builder requestBuilder = new Request.Builder().url("https://www.baidu.com");
+        Request request = requestBuilder.build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // TODO: 17-1-4  请求失败
+                Log.d(TAG, "onFailure: zhc");
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // TODO: 17-1-4 请求成功
+                //获得返回体
+                ResponseBody body = response.body();
+                String stringBody = body.string();
+            }
+        });
+    }
+}
